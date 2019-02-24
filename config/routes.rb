f5213a57f1b3b0e4bf6ea712f72bd3bec2113267
Juan Cardenas <juancarbe89@gmail.com>
2019-02-24 11:02:11 +1100

    adding FK to the models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all Cocktails
+  get '/restaurants' => 'restaurants#index'
+  # Create one Cocktail
+  get '/restaurants/new' => 'restaurants#new'
+  post '/restaurants' => 'restaurants#create'
+  # Read one Cocktail
+  get '/restaurants/:id' => 'restaurants#show'
+
+  resources :reviews, only: [:new, :create]
 end
